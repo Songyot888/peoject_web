783b62d2667e71cdf6e99eeb8b29a95fca1a0c35
Songyot888 <dog1235you@gmail.com>
2025-03-09 15:49:42 +0700

    p

--- templates/main_get.php
+++ templates/main_get.php
@@ -1,6 +1,10 @@
 <?php require_once 'header.php' ?>
 <?php
 $events = getAllEvents();
+$joined_events = getUserJoinedEvents($_SESSION['User_id']);
+$joined_event_ids = array_map(function($event) {
+    return $event['Event_id'];
+}, $joined_events);
 ?>
 <style>
     
@@ -103,8 +107,6 @@ body {
     </div>
 
     <div class="activity-container">
-     
-
         <?php if (!empty($events)): ?>
             <?php foreach ($events as $event): ?>
                 <div class="activity-card">
@@ -117,7 +119,11 @@ body {
                         <span class="like-count"></span>
                         <div class="content">
                             <button class="btn btn-primary">View</button>
-                            <button class="btn btn-success" onclick="window.location.href='/register_at?eid=<?php echo $event['Event_id']; ?>'">Sign</button>
+                            
+                            <?php if (!in_array($event['Event_id'], $joined_event_ids)): ?>
+                                <button class="btn btn-success" onclick="window.location.href='/register_at?eid=<?php echo $event['Event_id']; ?>'">Sign</button>
+                                
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
@@ -125,7 +131,6 @@ body {
         <?php else: ?>
             <p>ไม่มีข้อมูลกิจกรรม</p>
         <?php endif; ?>
-        
     </div>
 </div>
 
