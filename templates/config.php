777e18f1973d9fba0dd39bd7981e803c833ef30d
Songyot888 <dog1235you@gmail.com>
2025-03-27 09:03:57 +0700

    p

--- templates/config.php
+++ templates/config.php
@@ -5,7 +5,7 @@ function getConnection():mysqli
     $hostname = '45.64.187.212';
     $dbName = 'regacth_activity';
     $username = 'regacth_activity';
-    $password = 'nYFc4aftNzpV4KyABVyG';
+    $password = 'a4gBE@FMDsdKTF5';
     $conn = new mysqli($hostname, $username, $password, $dbName , 3306);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -15,5 +15,5 @@ function getConnection():mysqli
 
 require_once DATABASE_DIR . '/User.php';
 require_once DATABASE_DIR . '/Event.php';
-require_once DATABASE_DIR . '/Event_img.php';
+require_once DATABASE_DIR . '/Event_Img.php';
 require_once DATABASE_DIR . '/jointable.php';
