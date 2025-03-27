c43c8283e3f9e5936a45e09981b4708fcfa084c5
Songyot888 <dog1235you@gmail.com>
2025-03-27 09:05:12 +0700

    p

--- includes/index.php
+++ includes/index.php
@@ -3,10 +3,11 @@
 declare(strict_types=1);
 
 // Constant values for this project
-const INCLUDES_DIR = __DIR__ . '/../includes';
-const ROUTE_DIR = __DIR__ . '/../routes';
-const TEMPLATES_DIR = __DIR__ . '/../templates';
-const DATABASE_DIR = __DIR__ . '/../databases';
+const INCLUDES_DIR = __DIR__ . '/includes';
+const ROUTE_DIR = __DIR__ . '/routes';
+const TEMPLATES_DIR = __DIR__ . '/templates';
+const DATABASE_DIR = __DIR__ . '/databases';
+const CSS_DIR = __DIR__ . '/css';
 
 session_start();
 
@@ -15,8 +16,10 @@ require_once INCLUDES_DIR . '/router.php';
 require_once INCLUDES_DIR . '/view.php';
 require_once INCLUDES_DIR . '/config.php';
 
+
 // Call dispatch to handle requests
-const PUBLIC_ROUTES = ['/', '/login', '/singup'];
+// echo '$_SERVER["REQUEST_URI"]='.$_SERVER['REQUEST_URI'];
+const PUBLIC_ROUTES = ['/','/login','/register'];
 
 if (in_array(strtolower($_SERVER['REQUEST_URI']), PUBLIC_ROUTES)) {
     dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
